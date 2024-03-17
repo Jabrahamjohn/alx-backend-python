8aacd3228d6842443debf03e91a2352d84aa0de5
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-18 01:14:55 +0300

    this one here is gonna be a headache a bit

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""this initializes a script"""
+
+
+def vars(a:int, pi:float,i_understand_annotations:bool,school:str):
+    a = 1
+    pi = 3.14
+    school = "Holberton"
+    """this function vars annotates variables as specified
+    args:
+    a: integer value
+    pi: float value
+    i_understand annotations: boolean value
+    school: string value
+    Returns:
+    the variables
+    """
+    return int(a),float(pi),bool(i_understand_annotations),str(school)
