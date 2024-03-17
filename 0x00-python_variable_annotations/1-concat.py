ab4a9bf4039b7b0bca874bd3b216fdcf3810f55a
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-17 13:55:27 +0300

    lets concat these stuff

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" this is a typed annotated function concat that takes to strings and returns them"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    the function concat take 2 string  to be concatenate
+    
+    Args:
+        str1 string value 
+        str2 string value 
+
+    Returns:
+        this returns the concatenated strings
+    """
+    return str1 + str2
