0e92afe14226674bd009699f290f7109d0580e29
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-18 00:59:45 +0300

    we float on the string baby

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#usr/bin/env python3
+"""this is apython script for to string"""
+
+
+def to_str(n: float) -> str:
+    """this is a function that changes float to string
+    args:
+    n: float variable.
+    returns:
+    string representation of the float(n)
+    """
+    return str(n)
