5bac39cb7577731be83938e20ae8ea0899e160a1
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-18 01:18:47 +0300

    this i hope fixes it

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -5,6 +5,7 @@
 def vars(a:int, pi:float,i_understand_annotations:bool,school:str):
     a = 1
     pi = 3.14
+    i_understand_annotations = True
     school = "Holberton"
     """this function vars annotates variables as specified
     args:
