e81f35f4728480b7ca1a9b67ceec5f105d960e7a
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-18 00:28:23 +0300

    we floor it down

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """this is a typed annotated function floor"""
 def floor(n: float) -> int:
+
+    
     """this is a function floor that's a float
-    
     Args:
-    n: float.
-    
+    n: float
     Returns:
     the floor of the float n.
     """
