70c00a43178d8db924da19c6680905a6c0bdbd09
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-18 22:31:55 +0300

    i jinxed myself earlier

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -5,7 +5,7 @@ import asyncio
 import time
 
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
+wait_n = __import__('1-concurrent_coroutines.py').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,7 +5,7 @@ import asyncio
 from typing import List
 
 
-task_wait_random = __import__('3-tasks').task_wait_random
+task_wait_random = __import__('3-tasks.py').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
