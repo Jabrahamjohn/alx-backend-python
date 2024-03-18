e5d5f5386aa21f254cd4f39047c6ab768d027974
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-18 22:26:25 +0300

    weuh wacha tuu

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,14 +6,13 @@ import random
 
 wait_random = __import__('0-basic_async_syntax.py').wait_random
 
-async def wait_n(n: int , max_delay: int) -> float:
+
+async def wait_n(n: int , max_delay: int) -> list[float]:
     """this function imports the previous file first;
     waits for random delays concurrently
     Args:
     n: the number of concurrent wait operations
     max_delay: the max delays
     """
-    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
-    return sorted(delays)
-
-wait_random = __import__('0-basic_async_syntax.py').wait_random
+    delays_times = await asyncio.gather(*tuple(map(lambda _: wait_random(max_delay), range(n))))
+    return sorted(delays_times)
