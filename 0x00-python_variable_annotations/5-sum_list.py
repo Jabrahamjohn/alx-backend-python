c0c99be5e4dae4dca6acbd07ddf71f239f117dc9
Abraham John <jabrahamjohns@gmail.com>
2024-07-13 17:58:35 +0300

    let's reform the summation file and see

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
-"""scripting"""
+
+
+'''A module for returning the sum of a list'''
 
 
 from typing import List
 
-def sum_list(input_list: list[float]) -> float:
-    """Calculates the sum of all elements in a list of floats.
-    Args:
-    input_list: A list containing float values.
-    Returns:
-    The sum of all elements in the list as a float.
-    """
-    # Leverage the built-in sum function with type hinting
-    return sum(input_list)
+
+def sum_list(input_list: List[float]) -> float:
+    '''A function thst returns the sum of a list'''
+
+    total = 0.0
+    for i in input_list:
+        total += i
+    return total
+
+
+if __name__ == '__main__':
+    floats = [3.14, 1.11, 2.22]
+    floats_sum = sum_list(floats)
+    print(floats_sum == sum(floats))
+    print(sum_list.__annotations__)
+    print("sum_list(floats) returns {} which is a {}"
+          .format(floats_sum, type(floats_sum)))
